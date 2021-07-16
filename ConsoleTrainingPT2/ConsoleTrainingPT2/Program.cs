1eb55407ad6c77ae623b29af52689076f14e8dd2
sch3fr <sch3fr@gmail.com>
2021-07-16 09:19:33 +0200

    created a new file for methods training, the old project was getting messy

---
+++ ConsoleTrainingPT2/ConsoleTrainingPT2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleTrainingPT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  SayHi Method
+            SayHi("Frank", 20);
+            SayHi("Melanie", 18);
+            SayHi("Todd", 23);
+            */
+
+            /* Cubed Method
+            int cubedNumber;
+            cubedNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(cubed(cubedNumber));
+            */
+        }
+        static int cubed(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age + " years old.");
+        }
+    }
+}
