fa6d2b5f01900ab63a36e5328bb89db4d1c987d0
sch3fr <sch3fr@gmail.com>
2021-07-16 11:06:49 +0200

    finished todays revisions

--- ConsoleTrainingPT2/ConsoleTrainingPT2/Program.cs
+++ ConsoleTrainingPT2/ConsoleTrainingPT2/Program.cs
@@ -12,11 +12,208 @@ namespace ConsoleTrainingPT2
             SayHi("Todd", 23);
             */
 
+            //======================================================================================================================================
+
             /* Cubed Method
             int cubedNumber;
             cubedNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(cubed(cubedNumber));
             */
+
+            //======================================================================================================================================
+
+            /* Gender Choice
+            char choice;
+            Console.Write("Choose your gender. Press M for male, F for female, or N for not sure. ");
+            //choice = Convert.ToChar(Console.ReadKey());
+            choice = Console.ReadKey().KeyChar;
+            if (choice == 'm')
+            {
+                Console.WriteLine("\nSo you a stronk boy huh?");
+            }
+            else if (choice == 'f')
+            {
+                Console.WriteLine("\nHello there pretty lady ;)");
+            }
+            else if (choice == 'N')
+            {
+                Console.WriteLine("\nDon't worry, you'll figure that out :)");
+            }
+            else
+            {
+                Console.WriteLine("\nYou seem more than confused bruv.");
+            }
+            */
+
+            //======================================================================================================================================
+
+            /* IF ELSE Revision
+            bool isMale = true;
+            bool isTall = true;
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You a tall boi");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("Haha small boy");
+            }
+            else if (!isMale && isTall) 
+            { 
+                Console.WriteLine("ooh tall girl :)"); 
+            }
+            else
+            {
+                Console.WriteLine("Either youre not in a bois club or in a tall club. Or both. Sorry mate.");
+            }
+
+            if (isMale || isTall)
+            {
+                Console.WriteLine("Good for you");
+            }
+            else
+            {
+                Console.WriteLine("Haha, small girl");
+            }
+            */
+
+            //======================================================================================================================================
+
+            //Numbers Comparison
+            //Console.WriteLine(GetMax(4, 23, 12));
+
+            //======================================================================================================================================
+
+            /* Dumb calculator
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number :");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+            */
+
+            //======================================================================================================================================
+
+            //Switch revision
+            //Console.WriteLine(GetDay(14));
+
+            //======================================================================================================================================
+
+            /* While revision
+            int index = 1;
+            while (index < 11)
+            {
+                Console.WriteLine("Index value is " + index);
+                index++;
+            }
+            */
+
+            //======================================================================================================================================
+
+            /* Guessing game
+            string secretWord = "programming";
+            string guess = "";
+            int counter = 0;
+            int limit = 5;
+            bool outOfGuesses = false;
+
+            do
+            {
+                if (counter < limit) 
+                { 
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    counter++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            } while (guess != secretWord && !outOfGuesses);
+            if (outOfGuesses) { Console.WriteLine("You lose :("); }
+            else { Console.WriteLine("You Win!"); }
+            */
+
+            //======================================================================================================================================
+
+            int[] luckyNumbers = { 7, 13, 14, 42, 69, 420 };
+            for (int i = 0; i < luckyNumbers.Length; i++) 
+            {
+                Console.WriteLine("The number is  " + luckyNumbers[i]); 
+            }
+        }
+        static string GetDay(int dayNum)
+        {
+            string dayName = "";
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "pondělí";
+                    break;
+                case 1:
+                    dayName = "uterý";
+                    break;
+                case 2:
+                    dayName = "středa";
+                    break;
+                case 3:
+                    dayName = "čtvrtek";
+                    break;
+                case 4:
+                    dayName = "pátek";
+                    break;
+                case 5:
+                    dayName = "sobota";
+                    break;
+                case 6:
+                    dayName = "neděle";
+                    break;
+                default:
+                    Console.WriteLine("invalid day number");
+                    break;
+            }
+            return dayName;
+        }
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+            return result;
         }
         static int cubed(int num)
         {
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Listen I know I started 3 repos in like 1 month, but I just want repo to dump al
 Basically this is my training repository. I'm gonna dump here everything I do during the 100DaysOfCode challenge, that is not my projects. Plus this readme can be something like a daily log, right?
 ## 15.7.21
 I started this repo because I was working on Just-To-DoIt, but then I realized that I'm kinda lost and that I need to return to basics and fully understand the core concepts. Kinda wish I wasn't slacking so much during my programming basics course at uni.
+## 16.7.21
+I did some more of the C# revision with freeCodeCamp C# crash course. Even though I revised for as long as yesterday it feels like the time was much shorter. Tommorow I'm doing some theory stuff, I really need to clear those things up in those areas. I should finish the course tommorow and then I'll have to find something else to do. Either the course or just some project, we'll see.
