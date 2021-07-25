726b86e17fa5c5daa8ec943423be6e389dc890e7
sch3fr <sch3fr@gmail.com>
2021-07-25 10:10:55 +0200

    added project to try implement JTDI funcionality without destroying it; was to be deleted, changed my mind

---
+++ TBD/TBD/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TBD
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> allTasks { get; set; }
+        public MainWindow()
+        {
+            allTasks = new ObservableCollection<string>();
+            InitializeComponent();
+            this.DataContext = this;
+        }
+        private void buttonDelOne_Click(object sender, RoutedEventArgs e)
+        {
+            allTasks.RemoveAt(0);
+        }
+
+        private void buttonDelAll_Click(object sender, RoutedEventArgs e)
+        {
+            allTasks.Clear();
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            allTasks.Add("new task heehee");
+        }
+    }
+}
