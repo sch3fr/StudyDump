e3b987479c2ed52c8e6dfdb4240539c5e51ce4c0
sch3fr <50205729+sch3fr@users.noreply.github.com>
2022-07-03 16:33:31 +0200

    trying to understand strategy design pattern
    no luck, no fun :(

---
+++ Strategy/CalculateClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class CalculateClient
+    {
+        private ICalculateInterface calculateInterface;
+
+        public CalculateClient(ICalculateInterface strategy)
+        {
+            calculateInterface = strategy;
+        }
+        public int Calculate(int value1, int value2)
+        {
+            return calculateInterface.Calculate(value1, value2);
+        }
+    }
+}
---
+++ Strategy/ConcreteAgregate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Minus : ICalculateInterface
+    {
+        public int Calculate(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+    }
+    class Plus : ICalculateInterface
+    {
+        public int Calculate(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
